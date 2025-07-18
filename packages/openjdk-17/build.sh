87ef8dde13c17957698229701b40b2b956a36a92
Nikhilroy22 <nikhilroyfff@gmail.com>
2025-07-19 00:49:58 +0600

    first commit

--- packages/openjdk-17/build.sh
+++ packages/openjdk-17/build.sh
@@ -53,6 +53,9 @@ termux_pkg_auto_update() {
 
 termux_step_pre_configure() {
 	unset JAVA_HOME
+
+	# Patch all old CMakeLists.txt to require at least 3.5
+	find . -name CMakeLists.txt -exec sed -i 's/cmake_minimum_required(VERSION [0-9.]\+)/cmake_minimum_required(VERSION 3.5)/' {} +
 }
 
 termux_step_configure() {
